1f19c82f8af230edfc632101c8e17216f4f0c11a
salah-koulal <salah.rajawi289@gmail.com>
2023-11-21 12:29:26 +0100

    task-8

--- models/user.py
+++ models/user.py
@@ -3,21 +3,28 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+import os
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
+STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+
+    __tablename__ = "users"
+    if STORAGE == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', cascade="all,delete", backref="user")
-        reviews = relationship('Review', cascade="all,delete", backref="user")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship(
+            "Review",
+            cascade="all, delete,\
+                            delete-orphan",
+            backref="place",
+        )
     else:
         email = ""
         password = ""
