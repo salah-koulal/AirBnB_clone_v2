06ab38b72d37cd1dd17656404c360f6e3d90a3ef
mouadnait <moadnait6@gmail.com>
2023-11-23 14:13:40 +0100

    2. Console improvements

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, unique=True, primary_key=True)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """ Creates new instances of Base """
