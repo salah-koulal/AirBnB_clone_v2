8bd789ad8e0f5093915dd5e865fabe69f115ab91
salah-koulal <salah.rajawi289@gmail.com>
2023-12-07 23:01:34 +0100

    Task-0|Done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # script to set up clean server
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo "<!DOCTYPE html>
 <html>
     <head>
@@ -14,6 +12,7 @@ echo "<!DOCTYPE html>
     </head>
     <body>
         <h1>Welcome to AirBnB clone - Deploy static!</h1>
+        <h2>I'm Salah Koulal from  Alx Africa</h2>
     </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
