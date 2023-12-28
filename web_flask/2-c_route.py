21ba6aea388b09e98a2d6c30b3dfef56c35b37bf
salah-koulal <salah.rajawi289@gmail.com>
2023-12-28 16:52:35 +0100

    HbnbRoute

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/",  strict_slashes=False)
+def hello():
+    """start flask"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb",  strict_slashes=False)
+def hbnb():
+    """start flask"""
+    return "HBNB"
+
+
+@app.route("/c/<text>",  strict_slashes=False)
+def printC(text):
+    """display slag"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
