91afdaa38cc61991beac415202fec696af670e19
salah-koulal <salah.rajawi289@gmail.com>
2024-01-01 18:20:04 +0100

    Hopeitdone

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" doc flask"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def cities_states_list():
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    return render_template('10-hbnb_filters.html', amenities=amenities, states=states)
+
+
+@app.teardown_appcontext
+def closer(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
