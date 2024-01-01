d7e0dfafa3f3ff71979bc9aabd261211c5eafcf9
salah-koulal <salah.rajawi289@gmail.com>
2024-01-01 01:26:05 +0100

    listStates

--- models/base_model.py
+++ models/base_model.py
@@ -25,18 +25,19 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            # if self.id is None:
-            #     setattr(self, 'id', str(uuid.uuid4()))
-            # if self.created_at is None or self.updated_at is None:
-            #     self.created_at = self.updated_at = datetime.datetime.now()
+            if self.id is None:
+                setattr(self, 'id', str(uuid.uuid4()))
+            if self.created_at is None or self.updated_at is None:
+                self.created_at = self.updated_at = datetime.datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.datetime.now()
         
 
     def __str__(self):
